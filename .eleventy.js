434c5655cba79415b9685f7041d724286d12a472
supershem24 <107944240+supershem24@users.noreply.github.com>
2024-02-05 03:06:06 +0000

    Started blog post shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,11 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("blogPost", function(name, link) {
+    return`<section class="blogHolder">
+    <a href="${link}" title="${name}">${name}</a>
+    </section>`
+  });
+
 };
--- about.md
+++ about.md
@@ -3,5 +3,5 @@ layout: layout.liquid
 title: About
 ---
 
-# About the 11ty **Boilerplate**
+# About the Fiddlesbane
 <img class="about" alt="pine" src="/images/pine.png" width="50" />
